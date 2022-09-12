de4e6c14196d0d9e0033a497d413ff5c6f4e0487
felipysantos <felipys23@gmail.com>
2022-09-12 13:13:49 -0300

    update: user and institution test models and views updated

--- institutions/tests/test_models.py
+++ institutions/tests/test_models.py
@@ -3,26 +3,15 @@ from django.db import IntegrityError
 from django.test import TestCase
 from institutions.models import Institution
 from users.models import User
+
 from .mock import institution
+from model_bakery import baker
 
-
-class UserModelTest(TestCase):
+class InstitutionTestModel(TestCase):
+    
     @classmethod
-    def setUpTestData(cls) -> None:
-        cls.institution = Institution.objects.create(**institution)
-        
+    def setUp(cls):
+       cls.institution = baker.make('institutions.Institution')
 
-    def test_institution_max_length_attributes(self):
-        max_length_name = self.institution._meta.get_field("name").max_length
-        max_length_city = self.institution._meta.get_field("city").max_length
-        max_length_state = self.institution._meta.get_field("state").max_length
-        max_length_link = self.institution._meta.get_field("link").max_length
-        max_length_phone = self.institution._meta.get_field("phone").max_length
-        max_length_cep = self.institution._meta.get_field("cep").max_length
-        
-        self.assertEqual(max_length_name, 225)
-        self.assertEqual(max_length_city, 80)
-        self.assertEqual(max_length_state, 80)
-        self.assertEqual(max_length_link, 250)
-        self.assertEqual(max_length_phone, 80)
-        self.assertEqual(max_length_cep, 80)
+
+
--- users/tests/test_models.py
+++ users/tests/test_models.py
@@ -4,8 +4,54 @@ from django.test import TestCase
 from institutions.models import Institution
 from users.models import User
 from .mock import superuser, manager, reviewer_user, institution, student
-
-
+from model_bakery import baker
+class UserModelTest(TestCase):
+    
+    @classmethod
+    def setUp(cls):
+        cls.institution = baker.make('institutions.Institution')
+        cls.superuser = baker.make(
+        'users.User', 
+        username= "admin",
+        email="admin@email.com",
+        first_name= "Super",
+        last_name= "User",
+        password= "1234"
+    )
+        cls.manager = baker.make(
+        'users.User',
+        username= "manager",
+        email= "manager@email.com",
+        first_name= "Manager",
+        last_name= "Teste",
+        password= "1234",
+        is_superuser= True,
+        is_reviewer= True,
+        is_student= False,
+    )
+        cls.reviewer_user = baker.make(
+        'users.User',
+        username= "reviewerUser",
+        email= "reviewer@email.com",
+        first_name= "Reviewer",
+        last_name= "Test",
+        password= "1234",
+        is_superuser= False,
+        is_reviewer= True,
+        is_student= False,
+    )
+        cls.student = baker.make(
+        'users.User',
+        username= "student",
+        email= "student@email.com",
+        first_name= "Student",
+        last_name= "Test",
+        password= "1234",
+        is_superuser= False,
+        is_reviewer= False,
+        is_student= True,
+    
+    )
 class UserModelTest(TestCase):
     @classmethod
     def setUpTestData(cls) -> None:
