b499a798cb1a1f8da423581ecaf879499795e075
Anthoni <anthonifelipi@gmail.com>
2022-09-13 15:50:09 -0300

    feat: tests feedbacks

--- feedbacks/urls.py
+++ feedbacks/urls.py
@@ -3,9 +3,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("works/<str:work_id>/feedbacks/", views.FeedbackListCreateView.as_view()),
+    path(
+        "works/<str:work_id>/feedbacks/",
+        views.FeedbackListCreateView.as_view(),
+        name="feedback",
+    ),
     path(
         "works/<str:work_id>/feedbacks/<str:feedback_id>/",
         views.FeedbackRetrieveUpdateDeleteView.as_view(),
+        name="feedback_id",
     ),
 ]
